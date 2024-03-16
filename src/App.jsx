e5d2e20bf4d7bf0425c71aeadeb8f9931e410c2c
nekokaito <siddiqsazzad001@gmail.com>
2024-03-16 11:42:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './component/Header'
+import NavBar from './component/NavBar'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <NavBar></NavBar>
+     <Header></Header>
+    </>
+  )
+}
+
+export default App
