4537c438033c110e65cb58104ca6b63e68a2ca99
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-05-03 21:40:17 +0600

    changed Url

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -35,7 +35,7 @@ const NavBar = () => {
     <div className="dropdown dropdown-end">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
-          <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          <img alt="Tailwind CSS Navbar component" src="https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg" />
         </div>
       </div>
       <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
